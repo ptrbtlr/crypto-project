088ba4838a330b868224492f996c8f6b4e3fbca1
Cian Butler <ciandevelops@gmail.com>
2022-06-08 09:49:30 -0400

    tailwind works

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="">
+      <h1 className="text-red-500">Hello World</h1>
     </div>
   );
 }

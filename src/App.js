8020681e775cb17e48340e60b00480733aa6226b
Cian Butler <ciandevelops@gmail.com>
2022-06-08 13:50:41 -0400

    most routes done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { ThemeProvider} from './context/ThemeContext'
+import Home from './routes/Home';
+import SignIn from './routes/SignIn';
+import SignUp from './routes/SignUp';
+import Account from './routes/Account';
+
 function App() {
   return (
     <ThemeProvider>
       <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/signup' element={<SignUp/>} />
+        <Route path='/account' element={<Account/>} />
+      </Routes>
     </ThemeProvider>
   );
 }

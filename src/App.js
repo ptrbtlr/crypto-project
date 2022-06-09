f62baa9a41aee3609a1b3e3dbbd4b3f5115c7cc6
Cian Butler <ciandevelops@gmail.com>
2022-06-09 15:03:33 -0400

    footer created

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Account from './routes/Account';
 import CoinPage from './routes/CoinPage';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import Footer from './components/Footer';
 
 function App() {
   const [coins, setCoins] = useState([])
@@ -35,6 +36,7 @@ function App() {
           <Route path=':coinId'/>
         </Route>
       </Routes>
+      <Footer/>
     </ThemeProvider>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {FaFacebook, FaGithub, FaTiktok, FaTwitter} from 'react-icons/fa'
+import ThemeToggle from './ThemeToggle'
+
+
+const Footer = () => {
+  return (
+    <div>
+        <h1>Footer</h1>
+    </div>
+  )
+}
+
+export default Footer

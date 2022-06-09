debc41e6b8f280e4dab079b84a9f5b4460bcf162
Cian Butler <ciandevelops@gmail.com>
2022-06-09 15:00:52 -0400

    links for each coin

--- src/routes/CoinPage.js
+++ src/routes/CoinPage.js
@@ -3,12 +3,13 @@ import axios from "axios";
 import { Sparklines, SparklinesLine } from "react-sparklines";
 import { FaTwitter, FaFacebook, FaReddit, FaGithub } from "react-icons/fa";
 import DOMPurify from "dompurify";
+import { useParams } from "react-router-dom";
 
 const CoinPage = () => {
   const [coin, setCoin] = useState({});
+  const params = useParams();
 
-  const url =
-    "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true";
+  const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}?localization=false&sparkline=true`;
 
   useEffect(() => {
     axios.get(url).then((response) => {

a5fc149e6ee3f8bbe779df7b4e51f9c34788a8df
Cian Butler <ciandevelops@gmail.com>
2022-06-09 21:38:35 -0400

    account styled and coded

--- src/routes/Account.js
+++ src/routes/Account.js
@@ -3,21 +3,21 @@ import SavedCoin from '../components/SavedCoin'
 
 const Account = () => {
   return (
-    <div>
-      <div>
+    <div className='max-w-[1140px] mx-auto'>
+      <div className='flex justify-between items-center my-12 py-8 rounded-div'>
         <div>
-          <h1>Account</h1>
+          <h1 className='text-2xl font-bold'>Account</h1>
           <div>
-            <p>Welcome, User</p>
+            <p>Welcome, USER</p>
           </div>
         </div>
         <div>
-          <button>Sign Out</button>
+          <button className='border px-6 py-2 rounded-2xl shadow-lg hover:shadow-2xl'>Sign Out</button>
         </div>
       </div>
-      <div>
-        <div>
-          <h1>Watchlist</h1>
+      <div className='flex justify-between items-center my-12 py-8 rounded-div'>
+        <div className='w-full min-h-[300px]'>
+          <h1 className='text-2xl font-bold py-4'>Watchlist</h1>
           <SavedCoin/>
         </div>
       </div>

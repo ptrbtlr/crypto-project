7e0ac237b1f0fd24ae913a441b3b8be99ad932ac
Cian Butler <ciandevelops@gmail.com>
2022-06-10 11:11:08 -0400

    project complete

--- src/App.js
+++ src/App.js
@@ -1,42 +1,43 @@
-import { Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { ThemeProvider} from './context/ThemeContext'
-import Home from './routes/Home';
-import SignIn from './routes/SignIn';
-import SignUp from './routes/SignUp';
-import Account from './routes/Account';
-import CoinPage from './routes/CoinPage';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import Footer from './components/Footer';
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
+import Home from "./routes/Home";
+import SignIn from "./routes/SignIn";
+import SignUp from "./routes/SignUp";
+import Account from "./routes/Account";
+import CoinPage from "./routes/CoinPage";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
-  const [coins, setCoins] = useState([])
+  const [coins, setCoins] = useState([]);
 
-  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=10&page=1&sparkline=true'
+  const url =
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=cad&order=market_cap_desc&per_page=10&page=1&sparkline=true";
 
   useEffect(() => {
     axios.get(url).then((response) => {
-      setCoins(response.data)
-      
-    })
-  
-    
-  }, [url])
-  
+      setCoins(response.data);
+    });
+  }, [url]);
+
   return (
     <ThemeProvider>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home coins={coins}/>} />
-        <Route path='/signin' element={<SignIn/>} />
-        <Route path='/signup' element={<SignUp/>} />
-        <Route path='/account' element={<Account/>} />
-        <Route path='/coin/:coinId' element={<CoinPage />}>
-          <Route path=':coinId'/>
-        </Route>
-      </Routes>
-      <Footer/>
+      <AuthContextProvider>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home coins={coins} />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/coin/:coinId" element={<CoinPage />}>
+            <Route path=":coinId" />
+          </Route>
+        </Routes>
+        <Footer />
+      </AuthContextProvider>
     </ThemeProvider>
   );
 }
--- src/components/SavedCoin.js
+++ src/components/SavedCoin.js
@@ -1,15 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
+import { doc, onSnapshot, updateDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import { UserAuth } from "../context/AuthContext";
 
 const SavedCoin = () => {
   const [coins, setCoins] = useState([]);
+  const { user } = UserAuth();
+
+  useEffect(() => {
+    onSnapshot(doc(db, "users", `${user?.email}`), (doc) => {
+      setCoins(doc.data()?.watchList);
+    });
+  }, [user?.email]);
+
+  const coinPath = doc(db, "users", `${user?.email}`);
+  const deleteCoin = async (passedid) => {
+    try {
+      const result = coins.filter((item) => item.id !== passedid);
+      await updateDoc(coinPath, {
+        watchList: result,
+      });
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
   return (
     <div>
-      {coins.length === 0 ? (
+      {coins?.length === 0 ? (
         <p>
           You don't have any coins saved. Please save a coin to add it to your
-          watchlist. <Link to="/">Click here to search coins.</Link>
+          watch list. <Link to="/">Click here to search coins.</Link>
         </p>
       ) : (
         <table className="w-full border-collapse text-center">
@@ -21,24 +44,27 @@ const SavedCoin = () => {
             </tr>
           </thead>
           <tbody>
-            {coins.map((coin) => (
+            {coins?.map((coin) => (
               <tr key={coin.id} className="h-[60px] overflow-hidden">
                 <td>{coin?.rank}</td>
                 <td>
                   <Link to={`/coin/${coin.id}`}>
                     <div className="flex items-center">
-                      <img src={coin?.image} alt="/" className="w-8 mr-4" />
+                      <img src={coin?.image} className="w-8 mr-4" alt="/" />
                       <div>
                         <p className="hidden sm:table-cell">{coin?.name}</p>
                         <p className="text-gray-500 text-left text-sm">
-                          {coin?.symbol}
+                          {coin?.symbol.toUpperCase()}
                         </p>
                       </div>
                     </div>
                   </Link>
                 </td>
                 <td className="pl-8">
-                  <AiOutlineClose className="cursor-pointer" />
+                  <AiOutlineClose
+                    onClick={() => deleteCoin(coin.id)}
+                    className="cursor-pointer text-red-500"
+                  />
                 </td>
               </tr>
             ))}
